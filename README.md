e9204897ece477daa539cb25fe41e9b0d3803592
Gerrix90 <jahja.trifunovic@gmail.com>
2022-07-11 14:44:25 +0200

    animated canvas - drawPoints()

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-"# JetpackComposePlayground" 
+# Android Jetpack Compose Playground project
+
+	This project is source code for tutorials published on [YouTube](https://youtube.com/playlist?list=PL_BmKHKj5WdhIEClEa3PQ41iYMMSX1LsX). 
+	The project is made in Jetpack Compose.
+
--- app/src/main/java/com/jetpackcompose/playground/animations.kt
+++ app/src/main/java/com/jetpackcompose/playground/animations.kt
@@ -1,11 +1,15 @@
 package com.jetpackcompose.playground
 
+import android.annotation.SuppressLint
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.animateContentSize
 import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.rememberInfiniteTransition
 import androidx.compose.animation.core.tween
+import androidx.compose.animation.core.updateTransition
 import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
+import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
@@ -21,8 +25,13 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Path
+import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.unit.dp
+import org.json.JSONArray
 
 
 @Composable
